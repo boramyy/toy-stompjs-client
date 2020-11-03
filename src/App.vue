b831a9414fba117bb77398b033a623b8101d7f75
boramkim <boramyyo@gmail.com>
2020-11-03 17:57:18 +0900

    Add: 소켓 연결 테스트

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,42 @@
 <template>
   <div id="app">
     <h1>hello ~</h1>
+    <input type="text" v-model="message" />
+    <button @click="sendMsg">hoho</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import { Client } from '@stomp/stompjs';
+import { Stomp } from '@stomp/stompjs';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  // components: {
+  //   HelloWorld
+  // },
+  data(){
+    return {
+      message: ''
+    }
+  },
+  mounted() {
+
+    const client = Stomp.client('ws://192.168.1.118:15674/ws');
+    client.activate();
+
+    // client.subscribe(destination: string, callback: messageCallbackType, headers: StompHeaders)
+  },
+  methods: {
+    sendMsg() {
+      const req = new XMLHttpRequest()
+      req.open('get', `http://192.168.1.118:3000/send/${this.message}`, true)
+      req.send();
+    }
   }
 }
+
 </script>
 
 <style>
